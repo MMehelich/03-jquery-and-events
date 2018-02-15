b70cea4cef73348da7043d1464d07f766c6b5493
austincmatteson <austin.c.matteson@gmail.com>
2018-02-15 10:16:14 -0800

    author filter complete

--- austin-mikayla/scripts/articleView.js
+++ austin-mikayla/scripts/articleView.js
@@ -40,12 +40,14 @@ articleView.handleAuthorFilter = function() {
     if ($(this).val()) {
       // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
-      let $whereToGo = $(this).data('author');
+      let $whereToGo = $(this).val();
+      console.log($whereToGo);
       $('article').hide();
-      $('#' + $whereToGo).fadeIn(750);
+      $('article[data-author="' + $whereToGo + '"]').fadeIn(750);
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
-      !$('.template').show();
+      $('article').show();
+      $('.template').hide();
     }
     $('#category-filter').val('');
   });
